58d6497894d2eb494b63225344678fa2715f0a99
daniel attali <dattali@g.jct.ac.il>
2022-10-11 18:52:40 +0300

    first commit

---
+++ Tutorial_1/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Class1
+{
+	// this is the constructor of the class Class1
+	public Class1()
+	{
+	}
+
+	public static void Ex1()
+	{
+		int[] lst = new int[100];
+
+		for (int i = 0; i < 100; i++)
+		{
+            Random rd = new Random();
+			lst[i] = rd.Next(0, 100);
+        }
+
+		int largest = lst[0];
+		for (int i = 1; i < 100; i++)
+		{
+			if (largest < lst[i])
+			{
+				largest = lst[i];
+			}
+		}
+
+		Console.WriteLine(largest);
+	}
+
+	public static void Ex2()
+	{
+		int[] lst = new int[10];
+		for (int i = 0; i < 10; i++)
+		{
+			lst[i] = i;
+		}
+
+		for (int i = 0; i < 10; i++)
+		{
+			Console.Write(lst[i] + " ");
+		}
+
+		for (int i = 0; i < 5; i++)
+		{
+			int temp = lst[i];
+			lst[i] = lst[9 - i];
+			lst[9 - i] = temp;
+		}
+
+		Console.WriteLine();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(lst[i] + " ");
+        }
+    }
+}
--- Tutorial_1/Program.cs
+++ Tutorial_1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Class1.Ex2();
+    }
+
+    private static void Start()
+    {
+        Console.WriteLine("Welcome to the calculator");
+        Console.WriteLine("Enter a number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter operation: ");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        Console.WriteLine("Enter another number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        double ans = 0;
+
+        switch (operation)
+        {
+            case '+':
+                ans = num1 + num2;
+                break;
+            case '-':
+                ans = num1 - num2;
+                break;
+            case '*':
+                ans = num1 * num2;
+                break;
+            case '/':
+                ans = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("You Enter a wrong operation");
+                break;
+
+        }
+        Console.WriteLine("Answer: " + ans);
+    }
+}
